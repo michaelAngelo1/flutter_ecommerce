a5bb6c71edd0410c363642578b51ad0f107b4506
unknown <michaelangelo4489@gmail.com>
2023-04-08 11:45:38 +0700

    adding splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_ecommerce/constants.dart';
 import 'package:flutter_ecommerce/screens/home/home_screen.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(const MyApp());
@@ -18,7 +21,85 @@ class MyApp extends StatelessWidget {
       theme: Theme.of(context).copyWith(
         textTheme: Theme.of(context).textTheme.apply(bodyColor: kTextColor),
       ),
-      home: const HomeScreen(),
+      home: const SplashScreen(),
+    );
+  }
+}
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> with SingleTickerProviderStateMixin {
+  late AnimationController _animationController;
+  
+  @override
+  void initState() {
+    super.initState();
+    _animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 3),
+    );
+
+    _animationController.forward().then((_) => Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (context) => const HomeScreen())));
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: ScaleTransition(
+          scale: _animationController.drive(
+            Tween<double>(
+              begin: 1,
+              end: 1.5,
+            ).chain(
+              CurveTween(curve: Curves.ease),
+            ),
+          ),
+          child: Container(
+            decoration: BoxDecoration(
+              color: Color(0xFF3D82AE),
+              borderRadius: BorderRadius.circular(125),
+            ),
+            child: Padding(
+              padding: const EdgeInsets.all(kDefaultPadding),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Text(
+                    "Stylecommerce",
+                    style: GoogleFonts.lato(
+                      fontSize: 28.0,
+                      fontWeight: FontWeight.w700,
+                      color: Colors.white,
+                    )
+                  ),
+                  const SizedBox(height: 5.0),
+                  Text(
+                    "Style your day",
+                    style: GoogleFonts.lato(
+                      fontSize: 13.0,
+                      fontWeight: FontWeight.w500,
+                      color: Colors.white,
+                    )
+                  ),
+                ],
+              ),
+            ),
+          )
+        )
+      )
     );
   }
 }
